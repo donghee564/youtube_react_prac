b14bac695fc945fbdd3cdfffe1962ce7371a838a
unknown <poohaha8974@gmail.com>
2021-04-07 23:16:20 +0900

    youtube react practice

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,25 @@
-import './app.css';
+import { useEffect, useState } from "react";
+import "./app.css";
+import VideoList from "./components/video_list/video_list";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello :)</h1>
-    </div>
-  );
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAMnrUYVmM_sc9uLVxr9jxt1HAGP8Pp46U",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+  return <VideoList videos={videos} />;
 }
 
 export default App;
