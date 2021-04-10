4eec468f3f1ec31454fefe91f1ca26e8a87cee8f
unknown <poohaha8974@gmail.com>
2021-04-10 16:39:44 +0900

    make video item

--- src/app.jsx
+++ src/app.jsx
@@ -19,6 +19,7 @@ function App() {
       .then((result) => setVideos(result.items))
       .catch((error) => console.log("error", error));
   }, []);
+
   return <VideoList videos={videos} />;
 }
 
--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import VideoItem from "../video_item/video_item";
+import styles from "./video_list.module.css";
 
-const VideoList = (props) => (
-  <ul>
-    {props.videos.map((video) => (
-      <VideoItem key={video.id} video={video} />
-    ))}
-  </ul>
-);
+const VideoList = (props) => {
+  return (
+    <ul className={styles.video}>
+      {props.videos.map((video) => (
+        <VideoItem key={video.key} video={video} />
+      ))}
+    </ul>
+  );
+};
 
 export default VideoList;
